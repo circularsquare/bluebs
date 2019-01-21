2599de791a701f3e77f9985d9913fa1029789b90
abhatt349 <abhatt349@gmail.com>
2019-01-21 11:25:20 -0500

    added trainers just to see if i could

--- blueb/main.js
+++ blueb/main.js
@@ -1,13 +1,20 @@
 var bluebs = 0;
 var pickers = 0;
+var trainers = 0;
 var saveFile = {
   bluebs: bluebs,
-  pickers: pickers
+  pickers: pickers,
+  trainers: trainers
 }
 
 function pickBlueb(n){
   bluebs += n;
   document.getElementById("bluebs").innerHTML = bluebs;
+};
+
+function trainPicker(n){
+  pickers += n;
+  document.getElementById("pickers").innerHTML = pickers;
 };
 
 function buyPicker(){
@@ -21,23 +28,38 @@ function buyPicker(){
   document.getElementById('pickerCost').innerHTML = Math.floor(10*Math.pow(1.1, pickers));
 }
 
+function buyTrainer(){
+  var trainerCost = Math.floor(10 * Math.pow(1.125, trainers));
+  if (pickers >= trainerCost) {
+    trainers += 1;
+    pickers -= trainerCost;
+    document.getElementById('pickers').innerHTML = pickers;
+    document.getElementById('trainers').innerHTML = trainers;
+  }
+  document.getElementById('trainerCost').innerHTML = Math.floor(10*Math.pow(1.125, trainers));
+}
+
 //make sure all values r correct
 function reload(){
   document.getElementById('pickers').innerHTML = pickers;
   document.getElementById('bluebs').innerHTML = bluebs;
+  document.getElementById('trainers').innerHTML = trainers;
   document.getElementById('pickerCost').innerHTML = Math.floor(10*Math.pow(1.1, pickers));
+  document.getElementById('trainerCost').innerHTML = Math.floor(10*Math.pow(1.125, trainers));
 }
 
 //saving
 function save(){
   saveFile.bluebs=bluebs;
   saveFile.pickers=pickers;
+  saveFile.trainers=trainers;
   localStorage.setItem("save",JSON.stringify(saveFile));
 };
 function load(){
   var savegame = JSON.parse(localStorage.getItem("save"));
   if (typeof savegame.bluebs !== "undefined") bluebs = savegame.bluebs;
   if (typeof savegame.pickers !== "undefined") pickers = savegame.pickers;
+  if (typeof savegame.trainers !== "undefined") pickers = savegame.trainers;
   reload();
 };
 function reset(){localStorage.removeItem("save");}
@@ -51,4 +73,7 @@ function prettify(input){
 //every sec
 window.setInterval(function(){
   pickBlueb(pickers);
+}, 1000);
+window.setInterval(function(){
+  trainPicker(trainers);
 }, 1000);
