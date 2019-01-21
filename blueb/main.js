e13e928fa86a709027ac1fcc63436f51ee99fe5c
abhatt349 <abhatt349@gmail.com>
2019-01-21 12:49:31 -0500

    added sacrifices to the bluebLord

--- blueb/main.js
+++ blueb/main.js
@@ -1,10 +1,12 @@
 var bluebs = 0;
 var pickers = 0;
 var trainers = 0;
+var sacrifices = 0;
 var saveFile = {
   bluebs: bluebs,
   pickers: pickers,
-  trainers: trainers
+  trainers: trainers,
+  sacrifices: sacrifices
 }
 
 function pickBlueb(n){
@@ -18,7 +20,7 @@ function trainPicker(n){
 };
 
 function buyPicker(){
-  var pickerCost = Math.floor(10 * Math.pow(1.1, pickers));
+  var pickerCost = Math.floor(10 * Math.pow(1.1, pickers/((Math.pow(sacrifices,3)/3)+1)));
   if (bluebs >= pickerCost){
     pickers += 1;
     bluebs -= pickerCost;
@@ -29,7 +31,7 @@ function buyPicker(){
 }
 
 function buyTrainer(){
-  var trainerCost = Math.floor(10 * Math.pow(1.125, trainers));
+  var trainerCost = Math.floor(10 * Math.pow(1.125, trainers/((Math.pow(sacrifices,3)/5)+1)));
   if (pickers >= trainerCost) {
     trainers += 1;
     pickers -= trainerCost;
@@ -39,13 +41,29 @@ function buyTrainer(){
   document.getElementById('trainerCost').innerHTML = Math.floor(10*Math.pow(1.125, trainers));
 }
 
+function sacrifice(){
+  var sacrificeCost = Math.floor(Math.pow(10, sacrifices+2));
+  if (bluebs >= sacrificeCost) {
+    bluebs = 0;
+    pickers = 0;
+    trainers = 0;
+    sacrifices += 1;
+    document.getElementById('bluebs').innerHTML = bluebs;
+    document.getElementById('pickers').innerHTML = pickers;
+    document.getElementById('trainers').innerHTML = trainers;
+  }
+  document.getElementById('sacrificeCost').innerHTML = Math.floor(Math.pow(10, sacrifices+2));
+}
+
 //make sure all values r correct
 function reload(){
   document.getElementById('pickers').innerHTML = pickers;
   document.getElementById('bluebs').innerHTML = bluebs;
   document.getElementById('trainers').innerHTML = trainers;
+  document.getElementById('sacrifices').innerHTML = sacrifices;
   document.getElementById('pickerCost').innerHTML = Math.floor(10*Math.pow(1.1, pickers));
   document.getElementById('trainerCost').innerHTML = Math.floor(10*Math.pow(1.125, trainers));
+  document.getElementById('sacrificeCost').innerHTML = Math.floor(Math.pow(10, sacrifices+2));
 }
 
 //saving
@@ -53,6 +71,7 @@ function save(){
   saveFile.bluebs=bluebs;
   saveFile.pickers=pickers;
   saveFile.trainers=trainers;
+  saveFile.sacrifices=sacrifices;
   localStorage.setItem("save",JSON.stringify(saveFile));
 };
 function load(){
@@ -60,6 +79,7 @@ function load(){
   if (typeof savegame.bluebs !== "undefined") bluebs = savegame.bluebs;
   if (typeof savegame.pickers !== "undefined") pickers = savegame.pickers;
   if (typeof savegame.trainers !== "undefined") pickers = savegame.trainers;
+  if (typeof savegame.sacrifices !== "undefined") pickers = savegame.sacrifices;
   reload();
 };
 function reset(){localStorage.removeItem("save");}
