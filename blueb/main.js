b0dd841b4a5cd4e25e014e7542e4c4e24aa6dee5
Andrew <andrewxinchen@gmail.com>
2019-06-25 11:29:19 -0400

    node stuff actually works now

--- blueb/main.js
+++ blueb/main.js
@@ -1,5 +1,10 @@
+"use strict";
 
+var _react = _interopRequireDefault(require("react"));
 
+var _babel = _interopRequireDefault(require("babel"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 /*TODO:
 
@@ -14,245 +19,294 @@
 */
 var res = {
   'bluebs': 0,
-  'wood': 0,
+  'wood': 0
 };
-
 var maxbluebs = 1000;
 var maxwood = 1000;
 var jobs = {
   'pickers': 0,
   'woodpeckers': 0,
-  'unemployed': 0,
+  'unemployed': 0
 };
 var bld = {
-  'birdhouses' : 0,
+  'birdhouses': 0
 };
 var bldPrice = {
-  'birdhouses': {'wood':10}
-}
+  'birdhouses': {
+    'wood': 10
+  }
+};
 var birbs = 0;
 var maxbirbs = 2;
-
 var trainers = 0;
 var sacrifices = 0;
-
 var displayInfo = ["", "", "", "", "", "", "", "", "", ""];
 var saveFile = {
   bluebs: res['bluebs'],
   wood: res['wood'],
   pickers: jobs['pickers'],
-  woodpeckers: jobs['woodpeckers'],
-};
-
-//0 is plains
+  woodpeckers: jobs['woodpeckers']
+}; //0 is plains
 //1 is water
 //2 is city
 //3 is mountain
 //4 is forest
-var map = [
-            [3, 3, 0, 1, 1, 1, 1, 1, 0, 0],
-            [3, 3, 0, 1, 1, 1, 1, 1, 0, 0],
-            [3, 3, 0, 0, 1, 1, 1, 1, 0, 0],
-            [3, 4, 0, 0, 1, 1, 1, 1, 0, 0],
-            [3, 4, 4, 0, 0, 1, 1, 1, 1, 0],
-            [3, 4, 4, 0, 0, 0, 1, 1, 1, 1],
-            [4, 4, 0, 2, 0, 0, 1, 1, 1, 1],
-            [0, 0, 1, 1, 0, 0, 1, 1, 1, 1],
-            [0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
-            [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
-          ];
 
+var map = [[3, 3, 0, 1, 1, 1, 1, 1, 0, 0], [3, 3, 0, 1, 1, 1, 1, 1, 0, 0], [3, 3, 0, 0, 1, 1, 1, 1, 0, 0], [3, 4, 0, 0, 1, 1, 1, 1, 0, 0], [3, 4, 4, 0, 0, 1, 1, 1, 1, 0], [3, 4, 4, 0, 0, 0, 1, 1, 1, 1], [4, 4, 0, 2, 0, 0, 1, 1, 1, 1], [0, 0, 1, 1, 0, 0, 1, 1, 1, 1], [0, 0, 0, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1]];
 
-function pickBlueb(){
-  if (res['bluebs']<maxbluebs){res['bluebs']++; reload();}
-}
-function chopWood(){
-  if (res['wood']<maxwood){res['wood']++; reload();}
-}
-function harvest(){
-  if (res['bluebs']<maxbluebs){
-    if (res['bluebs']+jobs['pickers']>maxbluebs){res['bluebs']=maxbluebs;}
-    else {res['bluebs'] += jobs['pickers'];}
+function pickBlueb() {
+  if (res['bluebs'] < maxbluebs) {
+    res['bluebs']++;
+    reload();
   }
-  if (res['wood']<maxwood){
-    if(res['wood']+jobs['woodpeckers']>maxwood){res['wood']=maxwood;}
-    else {res['wood'] += jobs['woodpeckers']}
+}
+
+function chopWood() {
+  if (res['wood'] < maxwood) {
+    res['wood']++;
+    reload();
   }
+}
+
+function harvest() {
+  if (res['bluebs'] < maxbluebs) {
+    if (res['bluebs'] + jobs['pickers'] > maxbluebs) {
+      res['bluebs'] = maxbluebs;
+    } else {
+      res['bluebs'] += jobs['pickers'];
+    }
+  }
+
+  if (res['wood'] < maxwood) {
+    if (res['wood'] + jobs['woodpeckers'] > maxwood) {
+      res['wood'] = maxwood;
+    } else {
+      res['wood'] += jobs['woodpeckers'];
+    }
+  }
+
   reload();
-};
-function findBirb(){
-  if (birbs<maxbirbs && Math.random()<.2 && res['bluebs']>1){
+}
+
+;
+
+function findBirb() {
+  if (birbs < maxbirbs && Math.random() < .2 && res['bluebs'] > 1) {
     sendInfo("a birb lands on ur hand... it wants bluebs");
-    birbs+= 1;
+    birbs += 1;
     jobs['unemployed']++;
     reload();
   }
-};
+}
 
-function hire(type, number){
-  if(number>0){
-    if(jobs['unemployed']>number){
-      jobs[type]+=number;
-      jobs['unemployed']-=number;
-    } else if(jobs['unemployed']>0){
-      jobs[type]+=jobs['unemployed'];
-      jobs['unemployed']=0;
+;
+
+function hire(type, number) {
+  if (number > 0) {
+    if (jobs['unemployed'] > number) {
+      jobs[type] += number;
+      jobs['unemployed'] -= number;
+    } else if (jobs['unemployed'] > 0) {
+      jobs[type] += jobs['unemployed'];
+      jobs['unemployed'] = 0;
+    }
+  } else {
+    if (jobs[type] > -number) {
+      jobs[type] += number;
+      jobs['unemployed'] -= number;
+    } else if (jobs[type] > 0) {
+      jobs['unemployed'] += jobs[type];
+      jobs[type] = 0;
     }
   }
-  else{
-    if(jobs[type]>-number){
-      jobs[type]+=number;
-      jobs['unemployed']-=number;
-    } else if(jobs[type]>0){
-      jobs['unemployed']+=jobs[type];
-      jobs[type]=0;
-    }
-  }
+
   reload();
 }
-function build(type, number){
-  if(number<=0){bld[type]+=number;}
-  else{
-    for (i=0; i<number; i++){
+
+function build(type, number) {
+  if (number <= 0) {
+    bld[type] += number;
+  } else {
+    for (i = 0; i < number; i++) {
       var canAfford = true;
-      for (resource in bldPrice[type]){
-        if (res[resource]<bldPrice[type][resource]){
-          canAfford=false;
+
+      for (resource in bldPrice[type]) {
+        if (res[resource] < bldPrice[type][resource]) {
+          canAfford = false;
           break;
         }
       }
-      if(canAfford){
-        for (resource in bldPrice[type]){
-          res[resource]-=bldPrice[type][resource];
+
+      if (canAfford) {
+        for (resource in bldPrice[type]) {
+          res[resource] -= bldPrice[type][resource];
         }
-        bld[type]+=1;
+
+        bld[type] += 1;
+      } else {
+        break;
       }
-      else{break;}
     }
   }
+
   reloadCaps();
   reload();
 }
 
-function sacrifice(){
-  var sacrificeCost = Math.floor(Math.pow(10, sacrifices+2));
+function sacrifice() {
+  var sacrificeCost = Math.floor(Math.pow(10, sacrifices + 2));
+
   if (res['bluebs'] >= sacrificeCost) {
     res['bluebs'] = 0;
     jobs[pickers] = 0;
     trainers = 0;
     sacrifices += 1;
   }
-  reload();
-};
 
-//make sure all displayed values r correct
-function reloadCaps(){
-  maxbluebs=1000;
-  maxwood=1000;
-  maxbirbs = 2 + 2*bld['birdhouses'];
+  reload();
 }
-function reload(){
+
+; //make sure all displayed values r correct
+
+function reloadCaps() {
+  maxbluebs = 1000;
+  maxwood = 1000;
+  maxbirbs = 2 + 2 * bld['birdhouses'];
+}
+
+function reload() {
   document.getElementById('bluebs').innerHTML = res['bluebs'];
   document.getElementById('wood').innerHTML = res['wood'];
   document.getElementById('maxbluebs').innerHTML = maxbluebs;
   document.getElementById('maxwood').innerHTML = maxwood;
-
   document.getElementById('birbs').innerHTML = birbs;
   document.getElementById('maxbirbs').innerHTML = maxbirbs;
   document.getElementById('freeBirbs').innerHTML = jobs['unemployed'];
   document.getElementById('pickers').innerHTML = jobs['pickers'];
   document.getElementById('woodpeckers').innerHTML = jobs['woodpeckers'];
+  document.getElementById('birdhouses').innerHTML = bld['birdhouses'];
+}
 
-  document.getElementById('birdhouses').innerHTML=bld['birdhouses'];
+;
 
-  const element = <h1> hello world </h1>;
-  //ReactDOM.render(element, document.getElementById('test'));
-
-
-};
-
-function drawMap(){
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
+function drawMap() {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
   var pixSize = 12;
-  for (col = 0; col < map.length; col++){
-    for (row = 0; row < map[0].length; row++){
-      if(map[col][row]==0){ctx.fillStyle='#85ff3f';}//plain
-      else if(map[col][row]==1){ctx.fillStyle='#54afff';}//ocean
-      else if(map[col][row]==2){ctx.fillStyle='#d64017';}//city
-      else if(map[col][row]==3){ctx.fillStyle='#959699';}//mountain
-      else if(map[col][row]==4){ctx.fillStyle='#1cb525';}//forest
-      ctx.fillRect(pixSize*row, pixSize*col, pixSize, pixSize);
+  for (col = 0; col < map.length; col++) {
+    for (row = 0; row < map[0].length; row++) {
+      if (map[col][row] == 0) {
+        ctx.fillStyle = '#85ff3f';
+      } //plain
+      else if (map[col][row] == 1) {
+          ctx.fillStyle = '#54afff';
+        } //ocean
+        else if (map[col][row] == 2) {
+            ctx.fillStyle = '#d64017';
+          } //city
+          else if (map[col][row] == 3) {
+              ctx.fillStyle = '#959699';
+            } //mountain
+            else if (map[col][row] == 4) {
+                ctx.fillStyle = '#1cb525';
+              } //forest
+
+
+      ctx.fillRect(pixSize * row, pixSize * col, pixSize, pixSize);
     }
-  }
-  //ctx.fillStyle='#d64017';
+  } //ctx.fillStyle='#d64017';
   //ctx.fillRect(43, 43, 2, 2);
-};
 
+}
 
-//saving
-function save(){
-  saveFile.bluebs=res['bluebs'];
-  saveFile.wood=res['wood'];
-  saveFile.pickers=jobs['pickers'];
-  saveFile.woodpeckers=jobs['woodpeckers'];
-  localStorage.setItem("save",JSON.stringify(saveFile));
-};
-function load(){
+; //saving
+
+function save() {
+  saveFile.bluebs = res['bluebs'];
+  saveFile.wood = res['wood'];
+  saveFile.pickers = jobs['pickers'];
+  saveFile.woodpeckers = jobs['woodpeckers'];
+  localStorage.setItem("save", JSON.stringify(saveFile));
+}
+
+;
+
+function load() {
   var savegame = JSON.parse(localStorage.getItem("save"));
   if (typeof savegame.bluebs !== "undefined") res['bluebs'] = savegame.bluebs;
   if (typeof savegame.wood !== "undefined") res['wood'] = savegame.wood;
   if (typeof savegame.pickers !== "undefined") jobs['pickers'] = savegame.pickers;
   if (typeof savegame.woodpeckers !== "undefined") jobs['woodpeckers'] = savegame.woodpeckers;
   reload();
-};
-function reset(){localStorage.removeItem("save");}
+}
 
-//sends a string to the green dialog box on the bottom
-function sendInfo(instring){
+;
+
+function reset() {
+  localStorage.removeItem("save");
+} //sends a string to the green dialog box on the bottom
+
+
+function sendInfo(instring) {
   outInfo = "";
-  for (i = displayInfo.length-1; i>0; i-=1){
-    displayInfo[i] = displayInfo[i-1];
+
+  for (i = displayInfo.length - 1; i > 0; i -= 1) {
+    displayInfo[i] = displayInfo[i - 1];
   }
+
   displayInfo[0] = instring;
-  for (i = 0; i < displayInfo.length; i++){
-    outInfo += displayInfo[i] +"<br>";
+
+  for (i = 0; i < displayInfo.length; i++) {
+    outInfo += displayInfo[i] + "<br>";
   }
+
   document.getElementById('info').innerHTML = outInfo;
-};
-//for nums
-function prettify(input){
-    var output = Math.round(input * 1000)/1000;
-	return output;
-};
-//logic flow functions (onload, setinterval)
-function onLoad(){
-    openTab(event, "tent")
-    drawMap()
-    sendInfo("welcome!")
-  }
-//stuff to happen every sec should go in this loop
-window.setInterval(function(){
-    harvest();
-    findBirb();
-    tick();
+}
+
+; //for nums
+
+function prettify(input) {
+  var output = Math.round(input * 1000) / 1000;
+  return output;
+}
+
+; //logic flow functions (onload, setinterval)
+
+function onLoad() {
+  openTab(event, "tent");
+  drawMap();
+  sendInfo("welcome!");
+} //stuff to happen every sec should go in this loop
+
+
+window.setInterval(function () {
+  harvest();
+  findBirb();
+  tick();
 }, 1000);
 
 function tick() {
-  const element = <div><h1>Hello, world!</h1> <h2>It is {new Date().toLocaleTimeString()}.</h2></div>;
-  ReactDOM.render(element, document.getElementById('clock'));}
+  var element = _react["default"].createElement("div", null, _react["default"].createElement("h1", null, "Hello, world!"), " ", _react["default"].createElement("h2", null, "It is ", new Date().toLocaleTimeString(), "."));
 
-//some css stuff stolen from w3schools
+  ReactDOM.render(element, document.getElementById('clock'));
+} //some css stuff stolen from w3schools
+
+
 function openTab(evt, cityName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
+
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
+
   tablinks = document.getElementsByClassName("tablinks");
+
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
+
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+//# sourceMappingURL=main.js.map
